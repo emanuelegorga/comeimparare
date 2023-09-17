22730aca0640f29e61a890e8ecd1d906178500e7
Emanuele Gorga <emanuelegorga@gmail.com>
2023-09-17 17:16:04 +0100

    Heroku to Render hosting: prepare application with code required by Render

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  publishable_key: Rails.application.credentials.dig(:stripe, :publishable_key),
-  secret_key: Rails.application.credentials.dig(:stripe, :secret_key)
+  publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+  secret_key: ENV['STRIPE_SECRET_KEY']
 }
 
-Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key) 
+Stripe.api_key = ENV['STRIPE_SECRET_KEY']
